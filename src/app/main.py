7f2ca5205349a5b036e04bb57cd7b06235e92126
AdamNavarroLiriano <a.navarro0608@gmail.com>
2024-05-26 09:57:21 +0000

    working demo with pretrained models

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
+from ..models.pretrained import PRETRAINED_MODELS
+
 app = FastAPI(title="Language Translation API")
 
 origins = [
@@ -19,17 +21,34 @@ app.add_middleware(
 )
 
 
+def load_pretrained_model(src: str, tgt: str) -> tuple:
+    model_dict = PRETRAINED_MODELS[f"pretrained-{src}-{tgt}"]
+    tokenizer = model_dict["tokenizer"]
+    model = model_dict["model"]
+    return (model, tokenizer)
+
+
 @app.get("/")
 async def read_root():
     return {"welcome": "Language Translation API is running"}
 
 
 @app.post("/predict/", status_code=200)
-async def translate_query(request: str):
+async def translate_query(src_text: str, src: str, tgt: str):
+    # Load model
+    model, tokenizer = load_pretrained_model(src, tgt)
+
+    # Translate
+    translated = model.generate(
+        **tokenizer(src_text, return_tensors="pt", padding=True)
+    )
+    translated_text = [
+        tokenizer.decode(t, skip_special_tokens=True) for t in translated
+    ]
 
     return (
         {
             "status_code": 200,
-            "response": f"This is a placeholder response for {request}",
+            "response": f"{translated_text[0]}",
         },
     )
