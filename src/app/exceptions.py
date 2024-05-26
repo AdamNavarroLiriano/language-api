3f9cfa3de898295509eda9283126a7d366a770cc
AdamNavarroLiriano <a.navarro0608@gmail.com>
2024-05-26 10:06:33 +0000

    added language not supported exception

---
+++ src/app/exceptions.py
@@ -0,0 +1,11 @@
+class LanguagePairNotSupportedError(Exception):
+    def __init__(
+        self,
+        src: str,
+        tgt: str,
+        message="Translation from {src} to {tgt} is not supported.",
+    ):
+        self.src = src
+        self.tgt = tgt
+        self.message = message.format(src=src, tgt=tgt)
+        super().__init__(self.message)
